30752cb6691c4f6cd0256423d399a560314a036d
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-01-14 13:14:24 +0100

    Enhance main.cpp to display animal types and sounds
    Added output for animal types and sounds in main.

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -1,16 +1,16 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include <iostream> 
 int main()
 {
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-
-
-
-
-delete j;
-delete i;
-  
-return 0;
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound(); //will output the sound of the dog
+    delete j;//should not create a leak
+    delete i;
+    return 0;
 }
