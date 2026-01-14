23d9650c174b7427741099dc831793b137c54425
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-01-14 13:13:26 +0100

    Implement array of Animals with sound demonstrationa
    Added an array of Animal pointers to demonstrate polymorphism with Dog and Cat classes, including sound making and proper memory management.

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -1,12 +1,26 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+
 int main()
 {
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-delete j;//should not create a leak
-delete i;
-  
-return 0;
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    delete j;//should not create a leak
+    delete i;
+    Animal *arr[100];
+    for (int k = 0; k < 100; k++)
+    {
+        if (k % 2 == 0)
+            arr[k] = new Dog();
+        else
+            arr[k] = new Cat();
+    }
+    arr[4]->makeSound();
+    arr[72]->makeSound();
+    for (int k = 0; k < 100; k++)
+    {
+        delete arr[k];
+    }
+    return 0;
 }
