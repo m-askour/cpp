195bc0ef56da879cec3482edc0247e8601545ee6
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-05-15 16:43:30 +0100

    Delete modul01 directory

--- modul01/ex00/newZombie.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie( std::string name )
-{
-    Zombie *names =  new Zombie(name);
-    return (names);
-}
--- modul01/ex01/Zombie.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie::Zombie(std::string name) : name(name), num(10){}
-Zombie::~Zombie(){}
-
-void Zombie::announce()
-{
-    std::cout<<"this is the zombie allocat" <<name<<"\n";
-}
