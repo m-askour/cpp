9ab74078ddbc1f27c52ebbffd7e42c2540debc0a
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-07-10 17:39:46 +0100

    Update Span.hpp

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -12,6 +12,7 @@ public:
     Span(unsigned int N);
     ~Span();
     void addNumber(int num);
+    void Span::addNumbers(Iterator first, Iterator last);
     int shortestSpan();
     int longestSpan();
 };
