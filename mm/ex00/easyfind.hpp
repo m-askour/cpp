9fe6a1f39716f1896010d3bb37fab216cefd8221
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-07-15 15:11:54 +0100

    Update easyfind.hpp

--- mm/ex00/easyfind.hpp
+++ mm/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 #include<vector>
 #include<iterator>
 template<typename T>
-typename T::iterator easyfind(T continer, int integer)
+typename T::iterator easyfind(T &continer, int integer)
 {
     typename T::iterator it;
     it = std::find(continer.begin(),continer.end(),integer);//find return the iterator 
